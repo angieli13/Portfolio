5735be90467abc5eea4d3e294089aafa69abd1c5
AngieCombita <angie.combita@masu.mx>
2024-02-13 10:43:39 -0500

    feat(about): add animations and About Me section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,13 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        tuesday: ['"Tuesday Night"', "cursive"],
+      },
+    },
   },
   plugins: [],
 };
